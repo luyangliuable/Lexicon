f467316262b438471b8ab7c139f3364ead70ca8c
Luyang Liu <luyangl16@gmail.com>
2022-12-01 12:15:45 +1100

    Refined hero section of landing page. Refined nav bar.

--- components/pageComponents/IndexPage/HeroSection.tsx
+++ components/pageComponents/IndexPage/HeroSection.tsx
@@ -1,34 +1,76 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 import DualPanelSectionWrapper from "../../DualPanelSectionWrapper"
 
 const HeroSection = () => {
-  return (
-    <>
-      <DualPanelSectionWrapper
-        imageOnRight={true}
-        imageSrc={"homepage/doctor.jpg"}
-      >
-        <div className="py-12 px-6 sm:px-16  w-full flex items-center border-b">
-          <div>
-            <div className="flex mb-4 justify-center md:justify-start">
-              <p className="px-4 py-2 rounded-full bg-green-600 text-white">
-                Welcome to Lexicon
-              </p>
+
+    const getScrollLocation = () => {
+        console.log("Scroll");
+    };
+
+
+    useEffect(() => {
+        // Client-side-only code
+        window.onscroll = (e) => {
+            var scrolled = window.scrollY;
+            console.log(scrolled);
+        };
+    })
+
+    const [ heroState, setHeroState ] = useState({
+        opacity: 0,
+        marginTop: 12,
+    });
+
+    const animateHero = (newState) => {
+        const timer = setInterval(() => {
+
+            setHeroState(prev => {
+                if (prev.opacity >= newState.opacity || prev.marginTop <= newState.top) {
+                    clearInterval(timer);
+                    return {
+                        ...prev
+                    };
+                }
+
+                return {
+                    ...prev,
+                    opacity: prev.opacity + .1,
+                    marginTop: prev.marginTop - 1
+                };
+            });
+        }, 20);
+    }
+
+    useEffect(() => {
+        setTimeout(() => animateHero({opacity: 1, marginTop: 0}), 500);
+    }, [])
+
+
+    return (
+        <>
+            <div className="hero">
+                <div className="hero-content" style={{opacity: heroState.opacity, marginTop: `${heroState.marginTop}%`}}>
+                    <h1 className="text-center md:text-left text-blue-900 font-bold text-5xl mb-6 ">
+                        Australia's first medical search engine
+                    </h1>
+                    <p className="text-center md:text-left text-blue-900 text-lg">
+                        This platform enables clinicians to take more efficient and
+                        effective decisions by allowing them a quick and an easy access to
+                        medical tools and documents.
+                    </p>
+                    <div className="landing-page-button disable-text-highlight">
+                        Try Parsing Engine
+                    </div>
+                </div>
+
             </div>
-            <h1 className="text-center md:text-left text-blue-900 font-bold text-5xl mb-6 ">
-              Australia's first medical search engine
-            </h1>
-            <p className="text-center md:text-left text-blue-900 text-lg">
-              This platform enables clinicians to take more efficient and
-              effective decisions by allowing them a quick and an easy access to
-              medical tools and documents.
-            </p>
-          </div>
-        </div>
-      </DualPanelSectionWrapper>
-    </>
-  )
+            <div className="accordion-button-holder">
+                <div className="accordion-button-circle"></div>
+                <div className="accordion-button-circle"></div>
+            </div>
+        </>
+    )
 }
 
 export default HeroSection
